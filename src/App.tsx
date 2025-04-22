58fc53dbd9fc02c94591a10aee28cd2eb8fcb6dd
plabonislam <shahnurislamplabon@gmail.com>
2025-04-22 12:38:16 +0600

    biom unused import rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import viteLogo from "/vite.svg";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
@@ -6,7 +6,7 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  const _red: string = "red";
+  const red = "red";
 
   return (
     <>
