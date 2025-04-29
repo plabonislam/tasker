ccc56302f1dbce9ac087c152642e0590b7cb8a09
plabonislam <shahnurislamplabon@gmail.com>
2025-04-29 22:06:26 +0600

    date picker is added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import { Button } from "@/components/ui/button";
-
+import { Calendar } from '@/components/ui/calendar';
+import { useState } from 'react';
 function App() {
+  const [date, setDate] = useState<Date | undefined>(new Date())
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
       <Button>Click me</Button>
+      <Calendar
+      mode="single"
+      selected={date}
+      onSelect={setDate}
+      disabled={(date) => date < new Date()}
+      className="rounded-md border"
+    />
     </div>
   );
 }
