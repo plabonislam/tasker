13c2cebfcd71899b263544d53758b748152d6060
plabonislam <shahnurislamplabon@gmail.com>
2025-04-29 21:46:21 +0600

    shad button ui is added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
-function App() {
-  // const [count, setCount] = useState(0);
 
+function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
       <Button>Click me</Button>
