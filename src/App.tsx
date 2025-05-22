3320dbac0fb764caf607c4714c01ce39e8efe033
plabonislam <shahnurislamplabon@gmail.com>
2025-05-22 21:21:17 +0600

    footer ui is added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Navbar from "./features/Navbar";
 import HeroContent from "./features/hero-section/HeroContent";
+import Footer from "./features/Footer";
 function App() {
   return (
     <div className="flex min-h-screen flex-col">
       <Navbar />
       <HeroContent />
+      <Footer />
     </div>
   );
 }
