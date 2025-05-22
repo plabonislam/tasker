0e89b1ef260bcc35c8b9d9f959ca752a8760239e
plabonislam <shahnurislamplabon@gmail.com>
2025-05-22 22:30:03 +0600

    adding task components skeleton

---
+++ src/pages/ManageTask.tsx
@@ -0,0 +1,10 @@
+import Task from '../features/task/Task';
+import Layout from '@/app/Layout';
+function ManageTask() {
+  return (
+    <Layout>
+     <Task />
+    </Layout>
+  );
+}
+export default ManageTask;
