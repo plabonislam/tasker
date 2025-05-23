db333954afbc23b7c14cf0d583414b2809039982
plabonislam <shahnurislamplabon@gmail.com>
2025-05-23 14:11:00 +0600

    not found design is done

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+function NotFoundPage() {
+  return (
+    <div className="flex flex-col  flex-grow justify-center items-center ">
+      <h1 className="text-3xl font-bold mb-1">404 - Not Found!</h1>
+      <p className="text-xl font-bold">
+        The page you are looking for does not exist.
+      </p>
+    </div>
+  );
+}
+export default NotFoundPage;
